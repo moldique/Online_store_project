a9ce33e3cb1aa8f235ec2bbf608f823f4c4ed94c
Sergunin Vitaliy <sergunin_v@mail.ru>
2025-10-07 19:01:11 +0500

    Djando_4 промежуточный результат, создание нового приложения для блога

---
+++ blog/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+
+class BlogPost(models.Model):
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    preview = models.ImageField(upload_to='blog_previews/', blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    is_published = models.BooleanField(default=False)
+    views_count = models.PositiveIntegerField(default=0)
+
+    class Meta:
+        verbose_name = 'Блоговая запись'
+        verbose_name_plural = 'Блоговые записи'
+        ordering = ['-created_at']
+
+    def __str__(self) -> str:
+        return self.title
+
---
+++ blog/views.py
@@ -0,0 +1,52 @@
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from .models import BlogPost
+
+
+class BlogPostListView(ListView):
+    model = BlogPost
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+
+    def get_queryset(self):
+        # На списке показываем только опубликованные
+        return BlogPost.objects.filter(is_published=True)
+
+
+class BlogPostDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
+
+    def get_object(self, queryset=None):
+        obj = super().get_object(queryset)
+        # Инкремент счётчика просмотров
+        BlogPost.objects.filter(pk=obj.pk).update(views_count=models.F('views_count') + 1)
+        obj.refresh_from_db(fields=['views_count'])
+        return obj
+
+
+class BlogPostCreateView(CreateView):
+    model = BlogPost
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content', 'preview', 'is_published']
+
+    def get_success_url(self):
+        return reverse_lazy('blog:post_detail', kwargs={'pk': self.object.pk})
+
+
+class BlogPostUpdateView(UpdateView):
+    model = BlogPost
+    template_name = 'blog/post_form.html'
+    fields = ['title', 'content', 'preview', 'is_published']
+
+    def get_success_url(self):
+        # После редактирования переадресуем на страницу отредактированной записи
+        return reverse_lazy('blog:post_detail', kwargs={'pk': self.object.pk})
+
+
+class BlogPostDeleteView(DeleteView):
+    model = BlogPost
+    template_name = 'blog/post_confirm_delete.html'
+    success_url = reverse_lazy('blog:post_list')
+
